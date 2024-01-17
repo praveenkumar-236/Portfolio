2a6703d58922ce20df8302a2ae657a42790ebb41
praveenkumar-236 <praveenparker236@gmail.com>
2024-01-18 00:22:13 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  }
-  
-  module.exports = nextConfig
- 
+  // Your existing configuration here
+
+  // Add the following line to enable export
+  // This assumes you want to export the static HTML files
+  // If you have other configurations, you may need to adjust accordingly
+  output: {
+    // Set to true to enable static HTML export
+    export: true,
+  },
+};
+
+module.exports = nextConfig;
